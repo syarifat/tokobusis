5b29c4d06ac1c89daef70183c96d9704236dff5d
syarifat <syarifahsanit@gmail.com>
2026-03-31 14:30:11 +0700

    revisi penambahan pilihan antar, ambil dan trnasfer cash 4

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->grou
     Route::delete('/pelanggan/{pelanggan}', [PelangganController::class, 'destroy'])->name('pelanggan.destroy');
     Route::get('/laporan', [LaporanController::class, 'index'])->name('laporan.index');
     Route::get('/laporan/pdf', [LaporanController::class, 'exportPdf'])->name('laporan.pdf');
-    Route::post('/admin/pesanan/{id}/bayar-tunai', [AdminPesanan::class, 'bayarTunai'])->name('admin.pesanan.bayarTunai');
+    Route::post('/pesanan/{id}/bayar-tunai', [AdminPesanan::class, 'bayarTunai'])->name('pesanan.bayarTunai');
 });
 
 // Route khusus Pelanggan

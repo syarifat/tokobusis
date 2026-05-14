c2fa40203955dbef2551432e45378fade2fcc8cd
syarifat <syarifahsanit@gmail.com>
2026-05-14 15:38:07 +0700

    fitur rekomendasi barnag kualitas 1

--- resources/views/pelanggan/rekomendasi/index.blade.php
+++ resources/views/pelanggan/rekomendasi/index.blade.php
@@ -1,6 +1,4 @@
-@extends('layouts.app')
-
-@section('content')
+<x-app-layout>
 <div class="py-12 bg-gray-50 min-h-screen">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         
@@ -132,4 +130,4 @@
         @endif
     </div>
 </div>
-@endsection
+</x-app-layout>

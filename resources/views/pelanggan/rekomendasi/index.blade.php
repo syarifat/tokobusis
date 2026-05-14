8aa0cea1d49a56b27ade655b78c66038ef5ab7ce
syarifat <syarifahsanit@gmail.com>
2026-05-14 15:41:44 +0700

    fitur rekomendasi barnag kualitas 2

--- resources/views/pelanggan/rekomendasi/index.blade.php
+++ resources/views/pelanggan/rekomendasi/index.blade.php
@@ -68,7 +68,7 @@
                                                 <form action="{{ route('pelanggan.keranjang.store') }}" method="POST" class="mt-4">
                                                     @csrf
                                                     <input type="hidden" name="barang_id" value="{{ $item['rendah']->id }}">
-                                                    <input type="hidden" name="jumlah" value="1">
+                                                    <input type="hidden" name="qty" value="1">
                                                     <button type="submit" class="w-full bg-white border-2 border-green-500 text-green-600 hover:bg-green-50 font-bold py-2 px-4 rounded-lg transition text-sm flex justify-center items-center">
                                                         <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
                                                         Masukkan Keranjang
@@ -100,7 +100,7 @@
                                                 <form action="{{ route('pelanggan.keranjang.store') }}" method="POST" class="mt-4">
                                                     @csrf
                                                     <input type="hidden" name="barang_id" value="{{ $item['tinggi']->id }}">
-                                                    <input type="hidden" name="jumlah" value="1">
+                                                    <input type="hidden" name="qty" value="1">
                                                     <button type="submit" class="w-full bg-gradient-to-r from-yellow-500 to-amber-500 hover:from-yellow-600 hover:to-amber-600 text-white font-bold py-2 px-4 rounded-lg transition text-sm shadow-md flex justify-center items-center">
                                                         <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
                                                         Masukkan Keranjang
